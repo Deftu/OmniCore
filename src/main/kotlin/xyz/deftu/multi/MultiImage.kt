e89621ac8cd51d63ebb721239dcb1f807a2b0fc1
Deftu <deftudev@gmail.com>
2023-03-09 23:36:28 +0200

    Add MultiImage

---
+++ src/main/kotlin/xyz/deftu/multi/MultiImage.kt
@@ -0,0 +1,88 @@
+package xyz.deftu.multi
+
+//#if MC>=11600
+import net.minecraft.client.texture.NativeImage
+import java.awt.Color
+
+//#else
+//$$ import java.awt.image.BufferedImage
+//#endif
+
+/**
+ * Adapted from EssentialGG UniversalCraft under LGPL-3.0
+ * https://github.com/EssentialGG/UniversalCraft/blob/f4917e139b5f6e5346c3bafb6f56ce8877854bf1/LICENSE
+ */
+class MultiImage(
+    //#if MC>=11600
+    val image: NativeImage,
+    //#else
+    //$$ val image: BufferedImage,
+    //#endif
+) {
+    companion object {
+        @JvmStatic
+        @JvmOverloads
+        fun create(
+            width: Int,
+            height: Int,
+            clear: Boolean = true
+        ): MultiImage {
+            //#if MC>=11600
+            return MultiImage(NativeImage(width, height, clear))
+            //#else
+            //$$ return MultiImage(BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB))
+            //#endif
+        }
+    }
+
+    fun copyFrom(other: MultiImage) {
+        //#if MC>=11600
+        image.copyFrom(other.image)
+        //#else
+        //$$ image.graphics.drawImage(other.image, 0, 0, other.image.width, other.image.height, null)
+        //#endif
+    }
+
+    fun copy(): MultiImage {
+        //#if MC>=11600
+        return MultiImage(NativeImage(getWidth(), getHeight(), image.format.hasAlpha()))
+        //#else
+        //$$ return MultiImage(BufferedImage(getWidth(), getHeight(), image.type))
+        //#endif
+    }
+
+    fun deepCopy(): MultiImage {
+        //#if MC>=11600
+        return MultiImage(NativeImage(image.format, getWidth(), getHeight(), image.format.hasAlpha()).also { it.copyFrom(image) })
+        //#else
+        //$$ return MultiImage(BufferedImage(getWidth(), getHeight(), image.type).also { it.graphics.drawImage(image, 0, 0, image.width, image.height, null) })
+        //#endif
+    }
+
+    fun close() {
+        //#if MC>=11600
+        image.close()
+        //#endif
+    }
+
+    fun getPixel(x: Int, y: Int): Int {
+        //#if MC>=11600
+        return image.getColor(x, y)
+        //#else
+        //$$ return Integer.rotateLeft(image.getRGB(x, y), 8)
+        //#endif
+    }
+
+    fun setPixel(x: Int, y: Int, color: Int) {
+        //#if MC>=11600
+        image.setColor(x, y, color)
+        //#else
+        //$$ image.setRGB(x, y, Integer.rotateRight(color, 8))
+        //#endif
+    }
+
+    fun setPixel(x: Int, y: Int, color: Color) = setPixel(x, y, color.rgb)
+
+    fun getWidth() = image.width
+    fun getHeight() = image.height
+}
