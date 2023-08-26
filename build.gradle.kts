7a1a79055dbe7a8638f650b0f7cb5eac0a54df7e
Deftu <deftudev@gmail.com>
2023-08-26 09:23:28 +0200

    Add many more supported versions and change build file names

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,26 @@
 plugins {
-    id("xyz.deftu.gradle.multiversion-root")
+    java
+    kotlin("jvm")
+    `maven-publish`
+    id("xyz.deftu.gradle.multiversion")
+    id("xyz.deftu.gradle.tools")
+    id("xyz.deftu.gradle.tools.blossom")
+    id("xyz.deftu.gradle.tools.minecraft.loom")
+    id("xyz.deftu.gradle.tools.minecraft.api")
+    id("xyz.deftu.gradle.tools.minecraft.releases")
 }
 
-preprocess {
-    val fabric11904 = createNode("1.19.4-fabric", 11904, "yarn")
-    val fabric11903 = createNode("1.19.3-fabric", 11903, "yarn")
-    val fabric11902 = createNode("1.19.2-fabric", 11902, "yarn")
-    val fabric11802 = createNode("1.18.2-fabric", 11802, "yarn")
-    val fabric11701 = createNode("1.17.1-fabric", 11701, "yarn")
-    val fabric11605 = createNode("1.16.5-fabric", 11605, "yarn")
-    val fabric11502 = createNode("1.15.2-fabric", 11502, "yarn")
-    val forge11502 = createNode("1.15.2-forge", 11502, "srg")
-    val forge11202 = createNode("1.12.2-forge", 11202, "srg")
-    val forge10809 = createNode("1.8.9-forge", 10809, "srg")
-
-    fabric11904.link(fabric11903)
-    fabric11903.link(fabric11902, file("versions/1.19.3-1.19.2.txt"))
-    fabric11902.link(fabric11802)
-    fabric11802.link(fabric11701)
-    fabric11701.link(fabric11605, file("versions/1.17.1-1.16.5.txt"))
-    fabric11605.link(fabric11502)
-    fabric11502.link(forge11502)
-    forge11502.link(forge11202, file("versions/1.15.2-1.12.2.txt"))
-    forge11202.link(forge10809, file("versions/1.12.2-1.8.9.txt"))
+toolkitLoomApi.setupTestClient()
+if (mcData.isForge && mcData.version <= 1_15_02) {
+    toolkitLoomHelper.useKotlinForForge()
 }
 
-listOf(
-    "MavenLocalRepository",
-    "DeftuReleasesRepository",
-    "DeftuSnapshotsRepository"
-).forEach { repo ->
-    tasks.register("publishAllVersionsTo$repo") {
-        group = "publishing"
+dependencies {
+    implementation(kotlin("reflect"))
+//    modImplementation("xyz.deftu:TextCraft-${mcData.versionStr}-${mcData.loader.name}:1.0.1")
 
-        listOf(
-            "1.19.4-fabric",
-            "1.19.3-fabric",
-            "1.19.2-fabric",
-            "1.18.2-fabric",
-            "1.17.1-fabric",
-            "1.16.5-fabric",
-            "1.15.2-fabric",
-            "1.15.2-forge",
-            "1.12.2-forge",
-            "1.8.9-forge"
-        ).forEach { version ->
-            dependsOn(":$version:publishAllPublicationsTo$repo")
-        }
+    if (mcData.isFabric) {
+        modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.fabricApiVersion}")
+        modImplementation("net.fabricmc:fabric-language-kotlin:${mcData.fabricLanguageKotlinVersion}")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,29 +19,35 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("1.6.21")
-        id("xyz.deftu.gradle.multiversion-root") version("1.9.0")
+        kotlin("jvm") version("1.9.0")
+        id("xyz.deftu.gradle.multiversion-root") version("1.19.1")
     }
 }
 
 rootProject.name = "MultiCraft"
-rootProject.buildFileName = "build.gradle.kts"
+rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
     "1.8.9-forge",
     "1.12.2-forge",
-    "1.15.2-forge",
-    "1.15.2-fabric",
+    "1.16.5-forge",
     "1.16.5-fabric",
+    "1.17.1-forge",
     "1.17.1-fabric",
+    "1.18.2-forge",
     "1.18.2-fabric",
+    "1.19.2-forge",
     "1.19.2-fabric",
+    "1.19.3-forge",
     "1.19.3-fabric",
-    "1.19.4-fabric"
+    "1.19.4-forge",
+    "1.19.4-fabric",
+    "1.20.1-forge",
+    "1.20.1-fabric"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
         projectDir = file("versions/$version")
-        buildFileName = "../../version.gradle.kts"
+        buildFileName = "../../build.gradle.kts"
     }
 }
