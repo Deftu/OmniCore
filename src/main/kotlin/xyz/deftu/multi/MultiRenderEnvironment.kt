a30c9b3cf3f951dcd35d554dafffb69cd7b92451
Deftu <deftudev@gmail.com>
2023-08-28 18:08:43 +0200

    Update MultiRenderEnvironment with custom Preprocessor

--- src/main/kotlin/xyz/deftu/multi/MultiRenderEnvironment.kt
+++ src/main/kotlin/xyz/deftu/multi/MultiRenderEnvironment.kt
@@ -1,19 +1,19 @@
 package xyz.deftu.multi
 
-//#if MC<=11202
+//#if MC <= 1.12.2
 //$$ import net.minecraft.client.renderer.OpenGlHelper;
 //#endif
 
 object MultiRenderEnvironment {
     @JvmStatic fun isGL21Available() =
-        //#if MC>=11502
+        //#if MC >= 1.15.2
         true
         //#else
         //$$ OpenGlHelper.openGL21
         //#endif
 
     @JvmStatic fun isFramebufferEnabled() =
-        //#if MC>=11502
+        //#if MC >= 1.15.2
         true
         //#else
         //$$ OpenGlHelper.isFramebufferEnabled()
