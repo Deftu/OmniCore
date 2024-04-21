a8ac7af1174a03f8e6b8fff2bf13270997506ce1
Deftu <deftudev@gmail.com>
2024-04-21 16:38:38 +0200

    Update Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,8 +19,8 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("1.9.0")
         id("dev.deftu.gradle.multiversion-root") version("1.22.2")
+        kotlin("jvm") version("1.9.10")
     }
 }
 
