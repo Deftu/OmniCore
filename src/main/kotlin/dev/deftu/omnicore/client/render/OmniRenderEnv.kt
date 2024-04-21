a8dc14764e0ff909327b32b2f9a887062e9d06ae
Deftu <deftudev@gmail.com>
2024-04-21 16:45:30 +0200

    Replace MultiRenderEnvironment with OmniRenderEnv

--- src/main/kotlin/dev/deftu/omnicore/client/render/OmniRenderEnv.kt
+++ src/main/kotlin/dev/deftu/omnicore/client/render/OmniRenderEnv.kt
@@ -1,21 +1,25 @@
-package dev.deftu.omnicore
+package dev.deftu.omnicore.client.render
 
 //#if MC <= 1.12.2
 //$$ import net.minecraft.client.renderer.OpenGlHelper
 //#endif
 
-public object MultiRenderEnvironment {
-    @JvmStatic public fun isGL21Available(): Boolean =
+public object OmniRenderEnv {
+
+    @JvmStatic
+    public fun isGl21Available(): Boolean =
         //#if MC >= 1.15.2
         true
         //#else
         //$$ OpenGlHelper.openGL21
         //#endif
 
-    @JvmStatic public fun isFramebufferEnabled(): Boolean =
+    @JvmStatic
+    public fun isFramebufferEnabled(): Boolean =
         //#if MC >= 1.15.2
         true
         //#else
         //$$ OpenGlHelper.isFramebufferEnabled()
         //#endif
+
 }
