93d152dd48951d97f60362db2294e141c37eb292
Deftu <deftudev@gmail.com>
2024-04-28 17:29:05 +0200

    Decorate OmniCore obj with new annotations

--- src/main/kotlin/dev/deftu/omnicore/OmniCore.kt
+++ src/main/kotlin/dev/deftu/omnicore/OmniCore.kt
@@ -1,6 +1,22 @@
 package dev.deftu.omnicore
 
+import dev.deftu.omnicore.annotations.GameSide
+import dev.deftu.omnicore.annotations.Side
+
+@GameSide(Side.BOTH)
 public object OmniCore {
-    public val debug: Boolean
+
+    /**
+     * Represents OmniCore's current version.
+     */
+    @GameSide(Side.BOTH)
+    public const val VERSION: String = "@MOD_VERSION@"
+
+    /**
+     * Represents if OmniCore is in debug mode.
+     */
+    @GameSide(Side.BOTH)
+    public val isDebug: Boolean
         get() = System.getProperty("omnicore.debug")?.toBoolean() ?: false
+
 }
