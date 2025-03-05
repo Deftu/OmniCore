52d0f297bd1a9e298ab2056f7482e9481826cc3a
Deftu <me@deftu.dev>
2025-03-05 17:02:14 +0200

    Clean up and document OmniPackets

--- src/main/kotlin/dev/deftu/omnicore/common/OmniPackets.kt
+++ src/main/kotlin/dev/deftu/omnicore/common/OmniPackets.kt
@@ -1,8 +1,11 @@
 package dev.deftu.omnicore.common
 
+import dev.deftu.omnicore.annotations.GameSide
+import dev.deftu.omnicore.annotations.Side
 import io.netty.buffer.ByteBuf
 import java.util.function.BiConsumer
 import java.util.function.Function
+import kotlin.experimental.and
 
 //#if FORGE && MC <= 1.12.2
 //$$ import net.minecraftforge.fml.common.network.NetworkRegistry
@@ -20,7 +23,9 @@ public object OmniPackets {
     //$$ private val channels = mutableMapOf<ResourceLocation, SimpleNetworkWrapper>()
     //#endif
 
-    public fun writeVarInt(buf: ByteBuf, value: Int) {
+    @JvmStatic
+    @GameSide(Side.BOTH)
+    public fun writeVarInt(buf: ByteBuf, value: Int): ByteBuf {
         var currentValue = value
         while (currentValue and -128 != 0) {
             buf.writeByte(currentValue and 127 or 128)
@@ -28,28 +33,34 @@ public object OmniPackets {
         }
 
         buf.writeByte(currentValue)
+        return buf
     }
 
+    @JvmStatic
+    @GameSide(Side.BOTH)
     public fun readVarInt(buf: ByteBuf): Int {
         var result = 0
         var shift = 0
-        var byteValue: Int
+        var byteValue: Byte
 
         do {
-            if (shift >= 32) {
-                throw RuntimeException("VarInt is too large")
+            byteValue = buf.readByte()
+            result = result or ((byteValue.toInt() and 127) shl shift++ * 7)
+            if (shift > 5) {
+                throw RuntimeException("VarInt too big")
             }
-
-            byteValue = buf.readByte().toInt()
-            result = result or ((byteValue and 127) shl shift)
-            shift += 7
-        } while (byteValue and 128 != 0)
+        } while (shouldContinueReadingVarInt(byteValue))
 
         return result
     }
 
+    private fun shouldContinueReadingVarInt(byte: Byte): Boolean {
+        return (byte and 128.toByte()) == 128.toByte()
+    }
 
-    public fun writeString(buf: ByteBuf, string: String) {
+    @JvmStatic
+    @GameSide(Side.BOTH)
+    public fun writeString(buf: ByteBuf, string: String): ByteBuf {
         val bytes = string.toByteArray(Charsets.UTF_8)
         if (bytes.size > Short.MAX_VALUE) {
             throw IllegalArgumentException("String too big (was ${bytes.size} bytes, max ${Short.MAX_VALUE})")
@@ -57,8 +68,11 @@ public object OmniPackets {
 
         buf.writeVarInt(bytes.size)
         buf.writeBytes(bytes)
+        return buf
     }
 
+    @JvmStatic
+    @GameSide(Side.BOTH)
     public fun readString(buf: ByteBuf): String {
         val length = buf.readVarInt()
         val bytes = ByteArray(length)
@@ -66,13 +80,19 @@ public object OmniPackets {
         return String(bytes, Charsets.UTF_8)
     }
 
-    public fun <T> writeCollection(buf: ByteBuf, collection: Collection<T>, writer: BiConsumer<ByteBuf, T>) {
+    @JvmStatic
+    @GameSide(Side.BOTH)
+    public fun <T> writeCollection(buf: ByteBuf, collection: Collection<T>, writer: BiConsumer<ByteBuf, T>): ByteBuf {
         buf.writeVarInt(collection.size)
         for (item in collection) {
             writer.accept(buf, item)
         }
+
+        return buf
     }
 
+    @JvmStatic
+    @GameSide(Side.BOTH)
     public fun <T> readCollection(buf: ByteBuf, reader: Function<ByteBuf, T>): List<T> {
         val size = buf.readVarInt()
         return List(size) {
@@ -96,26 +116,32 @@ public object OmniPackets {
 
 }
 
+@GameSide(Side.BOTH)
 public fun ByteBuf.writeVarInt(value: Int) {
     OmniPackets.writeVarInt(this, value)
 }
 
+@GameSide(Side.BOTH)
 public fun ByteBuf.readVarInt(): Int {
     return OmniPackets.readVarInt(this)
 }
 
+@GameSide(Side.BOTH)
 public fun ByteBuf.writeString(string: String) {
     OmniPackets.writeString(this, string)
 }
 
+@GameSide(Side.BOTH)
 public fun ByteBuf.readString(): String {
     return OmniPackets.readString(this)
 }
 
+@GameSide(Side.BOTH)
 public fun <T> ByteBuf.writeCollection(collection: Collection<T>, writer: (ByteBuf, T) -> Unit) {
     OmniPackets.writeCollection(this, collection, writer)
 }
 
+@GameSide(Side.BOTH)
 public fun <T> ByteBuf.readCollection(reader: (ByteBuf) -> T): List<T> {
     return OmniPackets.readCollection(this, reader)
 }
