8edbb8ce883b14e78743fd73eff851274b8c1bdf
Deftu <me@deftu.dev>
2025-07-21 17:19:20 +0200

    Re-add CallbackInfoReturnable import

--- src/main/java/dev/deftu/omnicore/mixins/client/Mixin_Screen_CommandExecution.java
+++ src/main/java/dev/deftu/omnicore/mixins/client/Mixin_Screen_CommandExecution.java
@@ -72,6 +72,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 //#if MC >= 1.21.6
 import net.minecraft.client.gui.screen.Screen;
