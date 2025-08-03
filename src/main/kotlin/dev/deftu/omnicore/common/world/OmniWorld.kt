6fae56f15d6454b1d503f7d74b363239a1c29cf0
Deftu <me@deftu.dev>
2025-08-03 16:54:37 +0200

    This no longer has a reason to be `open`

--- src/main/kotlin/dev/deftu/omnicore/common/world/OmniWorld.kt
+++ src/main/kotlin/dev/deftu/omnicore/common/world/OmniWorld.kt
@@ -4,16 +4,16 @@ import dev.deftu.omnicore.common.OmniBlockPos
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
-public open class OmniWorld(public val vanilla: World) {
+public class OmniWorld(public val vanilla: World) {
 
     /** Whether this world instance exists on the logical client-side. False if this is the physical server. */
-    public open val isClient: Boolean
+    public val isClient: Boolean
         get() = vanilla.isClient
 
-    public open val dimension: OmniDimension
+    public val dimension: OmniDimension
         get() = OmniDimension.from(vanilla)
 
-    public open val spawnPosition: BlockPos
+    public val spawnPosition: BlockPos
         get() {
             //#if MC >= 1.19.2
             return vanilla.spawnPos
@@ -28,13 +28,13 @@ public open class OmniWorld(public val vanilla: World) {
             //#endif
         }
 
-    public open val worldTime: Long
+    public val worldTime: Long
         get() = vanilla.timeOfDay
 
-    public open val isDay: Boolean
+    public val isDay: Boolean
         get() = vanilla.isDay
 
-    public open val isNight: Boolean
+    public val isNight: Boolean
         get() {
             //#if MC >= 1.16.5
             return vanilla.isNight
@@ -43,43 +43,43 @@ public open class OmniWorld(public val vanilla: World) {
             //#endif
         }
 
-    public open val isRaining: Boolean
+    public val isRaining: Boolean
         get() = vanilla.isRaining
 
-    public open val isThundering: Boolean
+    public val isThundering: Boolean
         get() = vanilla.isThundering
 
-    public open val isClearWeather: Boolean
+    public val isClearWeather: Boolean
         get() = !isRaining && !isThundering
 
     /** Returns the chunk at the given **chunk coordinates** (__not__ block coords!) */
-    public open fun getChunk(chunkX: Int, chunkZ: Int): OmniChunkData? {
+    public fun getChunk(chunkX: Int, chunkZ: Int): OmniChunkData? {
         val chunk = vanilla.getChunk(chunkX, chunkZ) ?: return null
         val chunkPos = chunk.pos
         return OmniChunkData.from(this.vanilla, this.dimension, chunkPos)
     }
 
     /** Returns the chunk at the given **block coordinates** (__not__ chunk coords!) */
-    public open fun getChunkAt(pos: BlockPos): OmniChunkData? {
+    public fun getChunkAt(pos: BlockPos): OmniChunkData? {
         val chunk = vanilla.getChunk(pos) ?: return null
         val chunkPos = chunk.pos
         return OmniChunkData.from(this.vanilla, this.dimension, chunkPos)
     }
 
     /** Returns the chunk at the given **block coordinates** (__not__ chunk coords!) */
-    public open fun getChunkAt(x: Int, y: Int, z: Int): OmniChunkData? {
+    public fun getChunkAt(x: Int, y: Int, z: Int): OmniChunkData? {
         return getChunkAt(OmniBlockPos.from(x, y, z))
     }
 
-    public open fun getBiomeAt(pos: BlockPos): OmniBiomeData? {
+    public fun getBiomeAt(pos: BlockPos): OmniBiomeData? {
         return getChunkAt(pos)?.getBiomeAt(pos)
     }
 
-    public open fun getBiomeAt(x: Int, y: Int, z: Int): OmniBiomeData? {
+    public fun getBiomeAt(x: Int, y: Int, z: Int): OmniBiomeData? {
         return getBiomeAt(OmniBlockPos.from(x, y, z))
     }
 
-    public open fun isBlockLoaded(pos: BlockPos): Boolean {
+    public fun isBlockLoaded(pos: BlockPos): Boolean {
         //#if MC >= 1.16.5
         return vanilla.isPosLoaded(pos)
         //#else
@@ -87,7 +87,7 @@ public open class OmniWorld(public val vanilla: World) {
         //#endif
     }
 
-    public open fun isBlockLoaded(x: Int, y: Int, z: Int): Boolean {
+    public fun isBlockLoaded(x: Int, y: Int, z: Int): Boolean {
         return isBlockLoaded(OmniBlockPos.from(x, y, z))
     }
 
