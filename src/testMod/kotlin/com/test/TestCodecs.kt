04eb7da8a15bf592f2e4f3b389c5283451fc6173
Deftu <me@deftu.dev>
2025-08-09 22:14:03 +0200

    Add a bunch of new data to TestData and TestCodecs

--- src/testMod/kotlin/com/test/TestCodecs.kt
+++ src/testMod/kotlin/com/test/TestCodecs.kt
@@ -2,14 +2,23 @@ package com.test
 
 import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
+import dev.deftu.omnicore.common.codecs.OmniCodecs
 
 object TestCodecs {
 
     @JvmField
-    val TEST_CODEC = RecordCodecBuilder.create { instance ->
+    val TEST_CODEC: Codec<TestData> = RecordCodecBuilder.create { instance ->
         instance.group(
             Codec.STRING.fieldOf("name").forGetter(TestData::name),
-            Codec.INT.fieldOf("value").forGetter(TestData::value)
+            Codec.INT.fieldOf("value").forGetter(TestData::value),
+            OmniCodecs.IDENTIFIER.fieldOf("id").forGetter(TestData::id),
+            OmniCodecs.PATTERN.fieldOf("pattern").forGetter(TestData::pattern),
+            OmniCodecs.UUID_STRICT.fieldOf("strict_uuid").forGetter(TestData::strictUuid),
+            OmniCodecs.UUID_UNDASHED.fieldOf("undashed_uuid").forGetter(TestData::undashedUuid),
+            OmniCodecs.UUID_LENIENT.fieldOf("lenient_uuid").forGetter(TestData::lenientUuid),
+            OmniCodecs.UUID_LENIENT.fieldOf("other_lenient_uuid").forGetter(TestData::otherLenientUuid),
+            OmniCodecs.BLOCK_POS.fieldOf("block_pos").forGetter(TestData::blockPos),
+            OmniCodecs.INSTANT.fieldOf("instant").forGetter(TestData::instant)
         ).apply(instance, ::TestData)
     }
 
--- src/testMod/kotlin/com/test/TestData.kt
+++ src/testMod/kotlin/com/test/TestData.kt
@@ -1,10 +1,20 @@
 package com.test
 
+import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import java.time.Instant
+import java.util.UUID
+import java.util.regex.Pattern
+
 data class TestData(
     val name: String,
-    val value: Int
-) {
-    override fun toString(): String {
-        return "TestData(name='$name', value=$value)"
-    }
-}
+    val value: Int,
+    val id: Identifier,
+    val pattern: Pattern,
+    val strictUuid: UUID,
+    val undashedUuid: UUID,
+    val lenientUuid: UUID,
+    val otherLenientUuid: UUID,
+    val blockPos: BlockPos,
+    val instant: Instant,
+)
