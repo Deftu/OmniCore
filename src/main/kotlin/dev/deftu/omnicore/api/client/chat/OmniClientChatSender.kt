1aef2c77cf69b71164abc4f2b337742b4c495cf1
Deftu <me@deftu.dev>
2025-09-19 00:13:56 +0200

    Client chat queue -> sender. Allow sending immediately.

--- src/main/kotlin/dev/deftu/omnicore/api/client/chat/OmniClientChatSender.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/client/chat/OmniClientChatSender.kt
@@ -6,7 +6,7 @@ import dev.deftu.omnicore.api.client.player
 import dev.deftu.omnicore.api.eventBus
 import java.util.concurrent.ConcurrentLinkedQueue
 
-public object OmniClientChatQueue {
+public object OmniClientChatSender {
     private data class Entry(val message: String, val delay: Int)
 
     private val queue = ConcurrentLinkedQueue<Entry>()
@@ -20,29 +20,45 @@ public object OmniClientChatQueue {
                 return@on
             }
 
-            val player = player ?: return@on
+            player ?: return@on
             val entry = queue.peek() ?: return@on
             if (entry.delay % tickCounter != 0L) {
                 return@on
             }
 
-            //#if MC >= 1.19.4
-            player.networkHandler?.sendChatMessage(entry.message)
-            //#elseif MC >= 1.19.2
-            //$$ player.chatSigned(entry.message, null)
-            //#else
-            //$$ player.sendChatMessage(entry.message)
-            //#endif
-
+            send(entry.message)
             queue.remove()
             tickCounter = 0
         }
     }
 
+    /**
+     * Queues a chat message to be sent once any previously queued messages have been sent.
+     * Respects the queue per message; FIFO.
+     *
+     * @param message The message to send.
+     * @param delay The delay in ticks between sending this message and the previous one.
+     *             A delay of 0 means the message will be sent as soon as possible.
+     *             Must be non-negative.
+     */
     @JvmStatic
     @JvmOverloads
     public fun queue(message: String, delay: Int = 0) {
         require(delay >= 0) { "Delay must be non-negative" }
         queue.add(Entry(message, delay))
+    }
+
+    /**
+     * Sends a chat message immediately, regardless of any queued messages.
+     */
+    @JvmStatic
+    public fun send(message: String) {
+        //#if MC >= 1.19.4
+        player?.networkHandler?.sendChatMessage(message)
+        //#elseif MC >= 1.19.2
+        //$$ player?.chatSigned(message, null)
+        //#else
+        //$$ player?.sendChatMessage(message)
+        //#endif
     }
 }
