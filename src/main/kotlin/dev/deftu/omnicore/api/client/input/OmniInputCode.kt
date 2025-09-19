9c7107baa522f56985b76f9087ed6df5bfeb6709
Deftu <me@deftu.dev>
2025-09-19 14:22:43 +0200

    Codec for input codes

--- src/main/kotlin/dev/deftu/omnicore/api/client/input/OmniInputCode.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/client/input/OmniInputCode.kt
@@ -1,6 +1,14 @@
 package dev.deftu.omnicore.api.client.input
 
+import com.mojang.serialization.Codec
+
 public sealed interface OmniInputCode {
+    public companion object {
+        public val CODEC: Codec<OmniInputCode?> = Codec.INT.xmap({ code ->
+            OmniInputs.get(code)
+        }, { it?.code })
+    }
+
     public val code: Int
     public val isPressed: Boolean
 
