c8f6da361cef096566f63b6fb5c8cb7ffb71647c
Deftu <me@deftu.dev>
2025-09-22 19:49:52 +0200

    Add raw `gl` for GlCapabilities

--- src/main/kotlin/dev/deftu/omnicore/api/client/render/GlCapabilities.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/client/render/GlCapabilities.kt
@@ -2,7 +2,9 @@ package dev.deftu.omnicore.api.client.render
 
 //#if MC >= 1.16.5
 import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GLCapabilities
 //#else
+//$$ import org.lwjgl.opengl.ContextCapabilities
 //$$ import org.lwjgl.opengl.GLContext
 //#endif
 
@@ -11,6 +13,21 @@ import org.lwjgl.opengl.GL
 //#endif
 
 public object GlCapabilities {
+    @JvmStatic
+    public inline val gl:
+            //#if MC >= 1.16.5
+            GLCapabilities
+            //#else
+            //$$ ContextCapabilities
+            //#endif
+        get() {
+            //#if MC >= 1.16.5
+            return GL.getCapabilities()
+            //#else
+            //$$ return GLContext.getCapabilities()
+            //#endif
+        }
+
     @JvmStatic
     public val isGl21Available: Boolean
         get() {
@@ -23,13 +40,19 @@ public object GlCapabilities {
 
     @JvmStatic
     public val isGl3Available: Boolean
-        get() {
-            //#if MC >= 1.16.5
-            return GL.getCapabilities().OpenGL30
-            //#else
-            //$$ return GLContext.getCapabilities().OpenGL30
-            //#endif
-        }
+        get() = gl.OpenGL30
+
+    @JvmStatic
+    public val isGl31Available: Boolean
+        get() = gl.OpenGL31
+
+    @JvmStatic
+    public val isGl32Available: Boolean
+        get() = gl.OpenGL32
+
+    @JvmStatic
+    public val isGl33Available: Boolean
+        get() = gl.OpenGL33
 
     @JvmStatic
     public val isFramebufferEnabled: Boolean
