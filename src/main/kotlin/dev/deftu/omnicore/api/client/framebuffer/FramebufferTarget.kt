0ed6c20506966bc57953769cfa2f0c972e49aee2
Deftu <me@deftu.dev>
2025-09-22 04:10:53 +0200

    Fix OpenGL API issues (mostly in 1.8.9)

--- src/main/kotlin/dev/deftu/omnicore/api/client/framebuffer/FramebufferTarget.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/client/framebuffer/FramebufferTarget.kt
@@ -1,15 +1,6 @@
 package dev.deftu.omnicore.api.client.framebuffer
 
-import org.lwjgl.opengl.ARBFramebufferObject
-import org.lwjgl.opengl.EXTFramebufferBlit
-import org.lwjgl.opengl.EXTFramebufferObject
 import org.lwjgl.opengl.GL30
-
-//#if MC >= 1.16.5
-import org.lwjgl.opengl.GL
-//#else
-//$$ import org.lwjgl.opengl.GLContext
-//#endif
 
 public enum class FramebufferTarget {
     READ_WRITE,
@@ -18,36 +9,20 @@ public enum class FramebufferTarget {
 
     public val code: Int
         get() {
-            //#if MC >= 1.16.5
-            val caps = GL.getCapabilities()
-            //#else
-            //$$ val caps = GLContext.getCapabilities()
-            //#endif
-            if (caps.OpenGL30) {
-                return when (this) {
-                    READ -> GL30.GL_READ_FRAMEBUFFER
-                    WRITE -> GL30.GL_DRAW_FRAMEBUFFER
-                    READ_WRITE -> GL30.GL_FRAMEBUFFER
-                }
+            return when (this) {
+                READ -> GL30.GL_READ_FRAMEBUFFER
+                WRITE -> GL30.GL_DRAW_FRAMEBUFFER
+                READ_WRITE -> GL30.GL_FRAMEBUFFER
             }
+        }
 
-            if (caps.GL_ARB_framebuffer_object) {
-                return when (this) {
-                    READ -> ARBFramebufferObject.GL_READ_FRAMEBUFFER
-                    WRITE -> ARBFramebufferObject.GL_DRAW_FRAMEBUFFER
-                    READ_WRITE -> ARBFramebufferObject.GL_FRAMEBUFFER
-                }
+    public val binding: Int
+        get() {
+            return when (this) {
+                READ -> GL30.GL_READ_FRAMEBUFFER_BINDING
+                WRITE -> GL30.GL_DRAW_FRAMEBUFFER_BINDING
+                READ_WRITE -> GL30.GL_FRAMEBUFFER_BINDING
             }
-
-            if (caps.GL_EXT_framebuffer_blit && caps.GL_EXT_framebuffer_object) {
-                return when (this) {
-                    READ -> EXTFramebufferBlit.GL_READ_FRAMEBUFFER_EXT
-                    WRITE -> EXTFramebufferBlit.GL_DRAW_FRAMEBUFFER_EXT
-                    READ_WRITE -> EXTFramebufferObject.GL_FRAMEBUFFER_EXT
-                }
-            }
-
-            throw UnsupportedOperationException("Framebuffers are not supported on this context.")
         }
 
     public companion object {
--- src/main/kotlin/dev/deftu/omnicore/internal/client/framebuffer/FramebufferInternals.kt
+++ src/main/kotlin/dev/deftu/omnicore/internal/client/framebuffer/FramebufferInternals.kt
@@ -6,14 +6,6 @@ import org.jetbrains.annotations.ApiStatus
 import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL30
 
-//#if MC <= 1.12.2
-//$$ import net.minecraft.client.renderer.OpenGlHelper
-//#endif
-
-//#if MC < 1.21.5
-//$$ import com.mojang.blaze3d.platform.GlStateManager
-//#endif
-
 /**
  * Houses internal operations for framebuffers, mapping to the appropriate Minecraft version methods or OpenGL calls.
  *
@@ -26,29 +18,17 @@ import org.lwjgl.opengl.GL30
 public object FramebufferInternals {
     @JvmStatic
     public fun create(): Int {
-        //#if MC >= 1.17.1
         return GL30.glGenFramebuffers()
-        //#elseif MC >= 1.16.5
-        //$$ return GlStateManager.genFramebuffers()
-        //#else
-        //$$ return OpenGlHelper.glGenFramebuffers()
-        //#endif
     }
 
     @JvmStatic
     public fun bound(target: FramebufferTarget): Int {
-        return GL11.glGetInteger(target.code)
+        return GL11.glGetInteger(target.binding)
     }
 
     @JvmStatic
     public fun bind0(target: FramebufferTarget, id: Int) {
-        //#if MC >= 1.17.1
         GL30.glBindFramebuffer(target.code, id)
-        //#elseif MC >= 1.16.5
-        //$$ GlStateManager.bindFramebuffer(target.code, id)
-        //#else
-        //$$ OpenGlHelper.glBindFramebuffer(target.code, id)
-        //#endif
     }
 
     @JvmStatic
@@ -69,13 +49,7 @@ public object FramebufferInternals {
 
     @JvmStatic
     public fun delete(id: Int) {
-        //#if MC >= 1.17.1
         GL30.glDeleteFramebuffers(id)
-        //#elseif MC >= 1.16.5
-        //$$ GlStateManager.deleteFramebuffers(id)
-        //#else
-        //$$ OpenGlHelper.glDeleteFramebuffers(id)
-        //#endif
     }
 
     @JvmStatic
