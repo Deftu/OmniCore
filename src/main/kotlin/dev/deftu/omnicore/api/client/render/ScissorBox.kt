d2a468d19c3cf2a1adceac0f1c84b1e3d614b193
Deftu <me@deftu.dev>
2025-10-06 14:59:17 +0200

    Update ScissorBox.kt

--- src/main/kotlin/dev/deftu/omnicore/api/client/render/ScissorBox.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/client/render/ScissorBox.kt
@@ -1,6 +1,7 @@
 package dev.deftu.omnicore.api.client.render
 
 import dev.deftu.omnicore.api.client.render.stack.OmniMatrixStack
+import kotlin.math.ceil
 import kotlin.math.floor
 
 public data class ScissorBox(val x: Int, val y: Int, val width: Int, val height: Int) {
@@ -30,26 +31,13 @@ public data class ScissorBox(val x: Int, val y: Int, val width: Int, val height:
     }
 
     public fun transformQuickly(stack: OmniMatrixStack): ScissorBox {
-        val first = stack.transformPosition(left.toFloat(), top.toFloat())
-        val second = stack.transformPosition(right.toFloat(), bottom.toFloat())
-
-        val nx = floor(first.x).toInt()
-        val ny = floor(first.y).toInt()
-        val nw = floor(second.x - first.x).toInt()
-        val nh = floor(second.y - first.y).toInt()
-        return ScissorBox(nx, ny, nw, nh)
-    }
-
-    public fun transformEachVertex(stack: OmniMatrixStack): ScissorBox {
         val p1 = stack.transformPosition(left.toFloat(), top.toFloat())
-        val p2 = stack.transformPosition(right.toFloat(), top.toFloat())
-        val p3 = stack.transformPosition(left.toFloat(), bottom.toFloat())
-        val p4 = stack.transformPosition(right.toFloat(), bottom.toFloat())
+        val p2 = stack.transformPosition(right.toFloat(), bottom.toFloat())
 
-        val nx = floor(minOf(p1.x, p2.x, p3.x, p4.x)).toInt()
-        val ny = floor(minOf(p1.y, p2.y, p3.y, p4.y)).toInt()
-        val nx2 = floor(maxOf(p1.x, p2.x, p3.x, p4.x)).toInt()
-        val ny2 = floor(maxOf(p1.y, p2.y, p3.y, p4.y)).toInt()
+        val nx  = floor(minOf(p1.x, p2.x)).toInt()
+        val ny  = floor(minOf(p1.y, p2.y)).toInt()
+        val nx2 = ceil (maxOf(p1.x, p2.x)).toInt()
+        val ny2 = ceil (maxOf(p1.y, p2.y)).toInt()
         return ScissorBox(nx, ny, nx2 - nx, ny2 - ny)
     }
 }
