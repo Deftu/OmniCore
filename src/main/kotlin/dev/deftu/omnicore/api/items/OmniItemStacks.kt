206bbfe987ef204593d941bd37edf0053a85c631
Deftu <me@deftu.dev>
2025-10-07 07:03:50 +0200

    More item stack stuff + better Java names

--- src/main/kotlin/dev/deftu/omnicore/api/items/OmniItemStacks.kt
+++ src/main/kotlin/dev/deftu/omnicore/api/items/OmniItemStacks.kt
@@ -4,6 +4,7 @@ package dev.deftu.omnicore.api.items
 
 import net.minecraft.item.ItemStack
 
+@get:JvmName("count")
 public val ItemStack?.stackAmount: Int
     get() {
         //#if MC >= 1.12.2
@@ -14,6 +15,7 @@ public val ItemStack?.stackAmount: Int
         //#endif
     }
 
+@get:JvmName("isEmpty")
 public val ItemStack?.isStackEmpty: Boolean
     get() {
         if (this == null) {
@@ -26,3 +28,20 @@ public val ItemStack?.isStackEmpty: Boolean
         //$$ return count == 0
         //#endif
     }
+
+public fun ItemStack?.hasAtLeast(amount: Int): Boolean {
+    return stackAmount >= amount
+}
+
+public fun ItemStack?.hasLessThan(amount: Int): Boolean {
+    return stackAmount < amount
+}
+
+@JvmName("copy")
+public fun ItemStack?.copyStack(): ItemStack? {
+    if (this == null) {
+        return null
+    }
+
+    return this.copy()
+}
