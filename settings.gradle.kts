345ee4398af9e7fd94f5ee3f351fc7c5c95f22b2
Deftu <me@deftu.dev>
2025-11-07 09:29:19 +0200

    Bump DGT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version("2.2.10")
-        id("dev.deftu.gradle.multiversion-root") version("2.61.0")
+        id("dev.deftu.gradle.multiversion-root") version("2.62.0")
     }
 }
 
